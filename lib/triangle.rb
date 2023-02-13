999a918f3a3172ba25f308dcda418a22eba95a25
MORRIS <morris24kariuki@gmail.com>
2023-02-13 23:58:04 +0300

    assignment completed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
   # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize (a,b,c)
+    @a=a
+    @b=b
+    @c=c
+  end
+
+  def kind
+    if (@a <= 0 || @b <= 0 || @c <= 0) || ( @a + @b < @c || @a + @c < @b )
+      raise TriangleError
+    else 
+      if @a == @b && @a == @c 
+        :equilateral
+      elsif @a == @b || @a == @c || @b == @c 
+        :isosceles
+      elsif @a != @b && @b!= @c
+        :scalene
+      end
+    end
+
+    class TriangleError < StandardError
+      def error
+        "invalid triangle"
+      end
+    end
+
 end
+
+tri = Triangle.new 2, 2, 2
+tri = Triangle.new 3, 1, 1
+
